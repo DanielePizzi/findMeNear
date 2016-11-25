e044f4c13792a81e5e6d708b34a6f8d0d2662fc2
matt2710 <matteo.ercolani91@gmail.com>
2016-11-25 20:18:36 +0100

    token di sessione controller divisi

---
+++ src/main/java/findMeNear/controllers/LoginController.java
@@ -0,0 +1,47 @@
+package findMeNear.controllers;
+
+import javax.validation.Valid;
+
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import findMeNear.model.request.LoginRequest;
+import findMeNear.model.response.LoginResponse;
+import findMeNear.persistent.entity.User;
+import findMeNear.services.IServices;
+import findMeNear.services.impl.ServicesImpl;
+import findMeNear.utils.CryptPassword;
+import findMeNear.utils.SessionToken;
+
+@RestController
+public class LoginController {
+	
+private IServices services = new ServicesImpl();
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	@ResponseBody
+	public LoginResponse login(@Valid @RequestBody LoginRequest login, Errors errors){
+		LoginResponse response = new LoginResponse();
+		if(errors.hasErrors()){
+			response.setEsito(false);
+			return response;
+		}
+		
+		String email = login.getEmail();
+		String password = login.getPassword();
+		
+		User result = services.login(email);
+		if(CryptPassword.cryptWithMD5(password).equals(result.getPassword())){
+			response.setEsito(true);
+			response.setToken_sessione(SessionToken.getSessionToken());
+			return response;
+		}
+		response.setEsito(false);
+		return response; 
+	}
+	
+}
---
+++ src/main/java/findMeNear/model/response/LoginResponse.java
@@ -0,0 +1,19 @@
+package findMeNear.model.response;
+
+public class LoginResponse {
+	private boolean esito;
+	private String token_sessione;
+	
+	public boolean isEsito() {
+		return esito;
+	}
+	public void setEsito(boolean esito) {
+		this.esito = esito;
+	}
+	public String getToken_sessione() {
+		return token_sessione;
+	}
+	public void setToken_sessione(String token_sessione) {
+		this.token_sessione = token_sessione;
+	}
+}
