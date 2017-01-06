890aefa83d5640cce69feb9725c8b48dfa1243e6
matt2710 <matteo.ercolani91@gmail.com>
2017-01-06 21:21:56 +0100

    nullPointerException risolto

--- src/main/java/findMeNear/services/impl/ServicesImpl.java
+++ src/main/java/findMeNear/services/impl/ServicesImpl.java
@@ -76,7 +76,7 @@ public class ServicesImpl implements IServices{
 			return true;
 		}
 		
-		logger.debug(String.format("%s - %s:: l'utente non esiste [%s]",CLASS,method,user.toString()));
+		logger.debug(String.format("%s - %s:: l'utente non esiste",CLASS,method));
 		logger.debug(String.format("%s - %s:: return false",CLASS,method));
 		logger.debug(String.format("%s - %s::*****************************",CLASS,method));
 		logger.debug(String.format("%s - %s::           END",CLASS,method));
